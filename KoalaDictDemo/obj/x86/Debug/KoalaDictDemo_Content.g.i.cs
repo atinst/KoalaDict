b88d18a2de21ea9566d4da77801952cd315a9359
atinst <atinst@yahoo.com>
2017-11-14 15:51:40 +0800

    update

--- KoalaDictDemo/obj/x86/Debug/KoalaDictDemo_Content.g.i.cs
+++ KoalaDictDemo/obj/x86/Debug/KoalaDictDemo_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("image/translatebtndown.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("koalaico.ico")]
 
 
